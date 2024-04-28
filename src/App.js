e9a01918882659be8265052e327c7f05010d1aa6
githubgajen <github.gajen@gmail.com>
2024-04-27 20:11:20 -0400

    downloaded json-server implementation build from vercel, created new repo and push to same github repo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
+<<<<<<< HEAD
 import { useState } from 'react'
+=======
+import { useState, useEffect } from 'react'
+>>>>>>> 8d0f538 (downloaded json-server implementation build from vercel, created new repo and push to same github repo)
 import ReactLogo from './components/ReactLogo';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
@@ -7,6 +11,7 @@ import AddTask from './components/AddTask';
 
 
 const App = () => {
+<<<<<<< HEAD
     const [showAddTask, setShowAddTask] = useState(false)
 
     const [tasks, setTasks] = useState([
@@ -46,6 +51,81 @@ const App = () => {
     setTasks(
       tasks.map((task)=>
         task.id === id ? { ...task, reminder: !task.reminder } : task 
+=======
+  const [showAddTask, setShowAddTask] = useState(false)
+
+  const [tasks, setTasks] = useState([])  //empty array
+
+  useEffect(() => {
+    const getTasks = async() =>{
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)//add in state
+    }
+    getTasks()
+  },[])
+
+
+  // Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+    return data
+  }
+
+// Fetch Tasks
+const fetchTask = async (id) => {
+  const res = await fetch(`http://localhost:5000/tasks/${id}`)
+  const data = await res.json()
+  return data
+}
+
+    // Add Task
+    const addTask = async (task) => {
+    //const newTask = {id:3,...task}
+
+    const res = await fetch('http://localhost:5000/tasks',{
+      method: 'POST',
+      headers:{
+        'Content-type': 'application/json'
+      },
+      body:JSON.stringify(task)
+    })                          
+    const data = await res.json()
+
+    setTasks([...tasks,data]) 
+    //json-server creates id automatically
+
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newTask = { id, ...task }
+    // setTasks([...tasks, newTask])
+  }
+
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`,{
+    method:'DELETE',
+  })
+  setTasks(tasks.filter((task)=> task.id !== id))
+  }
+
+  const toggleReminder = async (id) =>{
+    //getting task, creating a task and putting it in a variable
+    const taskToToggle = await fetchTask(id)
+    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`,{
+      method:'PUT',
+      headers:{
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updatedTask)
+    })
+
+    const data = await res.json()
+
+    setTasks(
+      tasks.map((task)=>
+        task.id === id ? { ...task, reminder: data.reminder } : task 
+>>>>>>> 8d0f538 (downloaded json-server implementation build from vercel, created new repo and push to same github repo)
     )
     )
   }
